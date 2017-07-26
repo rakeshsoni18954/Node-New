40aebe27e119194b4ad20aea6b80096f7b691445
rakeshsoni18954 <rakesh.soni@venturepact.com>
2017-07-26 15:11:04 +0530

    [Routing]: structural proto-type

---
+++ config/response.js
@@ -0,0 +1,14 @@
+module.exports = {
+    ok: (res,data) => {
+        return res.status(200).json({
+            success: true,
+            data:data
+        });
+    },
+    error: () => {
+
+    },
+    unauthorized: () => {
+
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-index.js
+const express = require('express');   // framework
+const http = require('http');         // protocol 
+const path = require('path');         // node module to resolve paths
+const app = express();
+const port = process.env.PORT || 7555;
+const db = require('./config/db');
+//require('./routes/route-conf')(app);   // where routes will be defined
+require('./routes/route-list')(app);   // where routes will be defined
+//
+
+
+http.createServer(app).listen(port);
